08517a06084d3814a98fe38606df3f8e02dcf5f8
paladox <paladox@users.noreply.github.com>
2024-12-08 10:45:24 +0000

    Replace usages of deprecated ILoadBalancer::getConnectionRef() (#6)

--- SaneCase.php
+++ SaneCase.php
@@ -6,7 +6,7 @@ class SaneCase {
 	public static function onBeforeDisplayNoArticleText( $article ) {
 		$title = $article->getTitle();
 		$loadBalancer = MediaWikiServices::getInstance()->getDBLoadBalancer();
-		$dbr = $loadBalancer->getConnectionRef( DB_REPLICA );
+		$dbr = $loadBalancer->getConnection( DB_REPLICA );
 
 		$pageRow = $dbr->newSelectQueryBuilder()
 			->select( 'page_id' )
