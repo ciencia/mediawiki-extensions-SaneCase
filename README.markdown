f0c874466bae9ccd2d9613ac13a6100950c569ad
Jesús Martínez Novo <martineznovo@gmail.com>
2025-03-21 23:17:30 +0100

    New feature: try to fix broken links because of special chars
    This feature is disabled by default. To activate it,
    set `$wgSaneCaseAutofixSpecialCharBreak = true;`
    
    See README for more details.

--- README.markdown
+++ README.markdown
@@ -22,4 +22,29 @@ My response to that:
   that can be cached very well in Varnish or whatnot.
 - A 301 "Moved Permanently" redirect should be fine for search engines.
 
+**Note** This extension *doesn't make MediaWiki titles case-insensitive*! it only
+affects visitors following a link or manually typing the article title in the URL.
+Internal links are still case sensitive. See [Issue #3](https://github.com/ciencia/mediawiki-extensions-SaneCase/issues/3)
+for reasoning.
+
+Additional feature: try to fix broken links because of special chars
+----
+
+Sometimes, titles have some special characters, like parentheses, exclamation
+or question marks, or other punctuation or special characters. When sharing
+such links on IRC or social media, sometimes the link gets cut off at the
+presence of that special character, pointing to nowhere.
+
+With this new feature, the extension will try to find a title with the same prefix,
+*followed by a special character*, and if it finds it, you'll get redirected
+automatically to it.
+
+Note the emphasis in *followed by a special character*. Just displaying any title
+with the same prefix may be undesirable, since there may be more than one.
+Matching only titles followed by a special character will drastically reduce the
+chance to get a naive redirect to a very common prefix.
+
+This feature is disabled by default. To activate it,
+set `$wgSaneCaseAutofixSpecialCharBreak = true;` in LocalSettings.php
+
 Also see: https://www.mediawiki.org/wiki/Extension:SaneCase
--- SaneCase.php
+++ SaneCase.php
@@ -5,22 +5,49 @@ use MediaWiki\MediaWikiServices;
 class SaneCase {
 	public static function onBeforeDisplayNoArticleText( $article ) {
 		$title = $article->getTitle();
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'sanecase' );
+		$originalLength = mb_strlen( $title->getDBkey() );
 		$loadBalancer = MediaWikiServices::getInstance()->getDBLoadBalancer();
+		$tryAutoFixSpecialCharBreak = $config->get( 'SaneCaseAutofixSpecialCharBreak' );
 		$dbr = $loadBalancer->getConnection( DB_REPLICA );
-
-		$pageRow = $dbr->newSelectQueryBuilder()
-			->select( 'page_id' )
+		if ( $tryAutoFixSpecialCharBreak ) {
+			// Get chances to find one page with a special character matching, there may be several results that don't match the criteria
+			// while not getting a large result set
+			$limit = 20;
+			$titleCond = 'convert(page_title using utf8mb4) ' . $dbr->buildLike( $title->getDBkey(), $dbr->anyString() );
+		} else {
+			// Only want a strict case-insensitive match
+			$limit = 1;
+			$titleCond = [ 'convert(page_title using utf8mb4)' => $title->getDBkey() ];
+		}
+		// Get a list of pages which prefix matches the title
+		$res = $dbr->newSelectQueryBuilder()
+			->select( [ 'page_title', 'page_id' ] )
 			->from( 'page' )
-			->where( [
-				'page_namespace' => $title->getNamespace(),
-				'convert(page_title using utf8mb4)' => $title->getDBkey(),
-			] )
-			->caller( __METHOD__ )->fetchRow();
+			->where( [ 'page_namespace' => $title->getNamespace() ] )
+			->where( $titleCond )
+			->limit( $limit )
+			->caller( __METHOD__ )
+			->fetchResultSet();
 
-		if ( $pageRow ) {
-			$title = Title::newFromID( $pageRow->page_id );
-			header( 'HTTP/1.1 301 Moved Permanently' );
-			header( 'Location: ' . $title->getLocalURL() );
+		$found = false;
+		foreach ( $res as $row ) {
+			if ( !$tryAutoFixSpecialCharBreak ) {
+				$found = true;
+			} else if (
+				mb_strlen( $row->page_title ) > $originalLength &&
+				preg_match( '/^_*[^a-zA-Z0-9-_~$]/', mb_substr( $row->page_title, $originalLength ) )
+			) {
+				// the next character is a special one (not plain ascii "word" or safe punctuation)
+				// optionally prefixed by a space (underscore in db), since MW trims spaces at the end
+				$found = true;
+			}
+			if ( $found ) {
+				$title = Title::newFromID( $row->page_id );
+				header( 'HTTP/1.1 301 Moved Permanently' );
+				header( 'Location: ' . $title->getLocalURL() );
+				return;
+			}
 		}
 	}
 }
